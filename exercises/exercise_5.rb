175ba0f07587e916ef470b5d97589d26618d4d22
mike chui <mikel.k.khui@gmail.com>
2019-09-16 22:04:28 +0000

    chained .where and .count

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,8 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.sum(:annual_revenue)
+
+puts "Average revenue per store is $#{@total_revenue/Store.count}"
+
+puts "#{Store.where("annual_revenue >= ?", 1_000_000).count} store generates more than $1M in annnual revenue"
