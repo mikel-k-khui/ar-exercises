11c322f16f4c03f5b0827c031e3c9455a55e4658
mike chui <mikel.k.khui@gmail.com>
2019-09-16 21:53:12 +0000

    Used .create and reference from store.rb schema to create 3 stores

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -3,4 +3,39 @@ require_relative '../setup'
 puts "Exercise 1"
 puts "----------"
 
+# Schema was defined as Store per store.rb
+# create_table :stores do |t|
+#   t.column :name, :string
+#   t.column :annual_revenue, :integer
+#   t.column :mens_apparel, :boolean
+#   t.column :womens_apparel, :boolean
+#   t.timestamps null: false
+# end
+
 # Your code goes below here ...
+
+# Burnaby (annual_revenue of 300000, carries men's and women's apparel)
+store = Store.create(
+  name: "Burnaby",
+  annual_revenue: 300_000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+# Richmond (annual_revenue of 1260000 carries women's apparel only)
+store = Store.create(
+  name: "Richmond",
+  annual_revenue: 1_260_000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+# Gastown (annual_revenue of 190000 carries men's apparel only)
+store = Store.create(
+  name: "Gastown",
+  annual_revenue: 190_000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+puts "There is #{Store.count} stores in Vancouver"
