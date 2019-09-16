bdffdca3902cfe2ba82a0eb556660d9833c190d3
mike chui <mikel.k.khui@gmail.com>
2019-09-16 21:53:53 +0000

    Used .first and .where: latter returns an array isntead of a single object

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.first
+@store2 = Store.where(id: "2")
+@store1.update(name: "Canmore")
+
+# puts @store2.inspect
+# puts "What is in store 2? #{@store2[0].name}"
+puts "Store 1 is now #{@store1[:name]} and store 2 is still #{@store2[0].name}"
+
+puts 
