9ea5370b366afdb4968b3cebfa6167855074a981
mike chui <mikel.k.khui@gmail.com>
2019-09-16 21:54:51 +0000

    Used compounded .where and .order to query

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,41 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+# Surrey (annual_revenue of 224000, carries women's apparel only)
+store = Store.create(
+  name: "Surrey",
+  annual_revenue: 224_000,
+  mens_apparel: false,
+  womens_apparel: true
+)
+
+# Whistler (annual_revenue of 1900000 carries men's apparel only)
+store = Store.create(
+  name: "Whistler",
+  annual_revenue: 1_900_000,
+  mens_apparel: true,
+  womens_apparel: false
+)
+
+# Yaletown (annual_revenue of 430000 carries men's and women's apparel)
+store = Store.create(
+  name: "Yaletown",
+  annual_revenue: 430_000,
+  mens_apparel: true,
+  womens_apparel: true
+)
+
+@men_stores = Store.where(mens_apparel: true)
+                   .order(name: :asc)
+
+@men_stores.each do |s|
+  puts "#{s.name} has annual revenue of #{s.annual_revenue}"
+end
+
+@women_small_stores = Store.where("annual_revenue < ?", 1_000_000)
+                   .where(womens_apparel: true)
+                   .order(name: :asc)
+
+@women_small_stores.each do |s|
+  puts "#{s.name} seels #{s.womens_apparel ? "women's appareal" : "na"} has annual revenue of #{s.annual_revenue}"
+end
