e36e238aa34a1d3ebcb8d3bd91118e133365f821
Mike Chui <mikel.k.khui@gmail.com>
2019-09-16 23:42:20 -0600

    Added :restrict_with_error to depdendent to throw error if any employees were working at the store

--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,6 @@
 class Store < ActiveRecord::Base
-  has_many :employees, :dependent => :restrict_with_error
+  has_many :employees,
+    :dependent => :restrict_with_error
 
   # Stores must always have a name that is a minimum of 3 characters
   validates :name, length: { minimum: 3 }
